e87c3255b913b41052d0a44098a2eafac17520a8
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-03-12 11:50:56 +0000

    Removed unnecessary dependencies to unused packages
    Removed:
    - body-parser, it's a proxy, we don't mess with the body
    - serve-favicon, was required but not used
    - uglify-js, not used and we don't have templates anymore anyway

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
@@ -41,9 +39,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
