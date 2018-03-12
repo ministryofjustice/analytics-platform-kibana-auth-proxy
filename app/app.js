5d92dbc8e028a3a3647afb2707583960eb871054
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-03-12 11:42:48 +0000

    Removed views, public/static and pug dependency
    As /login will start to use Auth0' Hosted login page.

--- app/app.js
+++ app/app.js
@@ -34,16 +34,11 @@ passport.deserializeUser(function(user, done) {
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('combined'));
 app.use(cookieParser());
 app.use(session(config.session));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
@@ -57,28 +52,19 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
+// error handler
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  response = { 'error': err.message };
+
+  // development error handler include details (e.g. stacktrace)
+  if (app.get('env') === 'development') {
+    response['details'] = err;
+  }
+
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.set('Content-Type', 'application/json');
+  res.send(JSON.stringify(response));
 });
 
 
